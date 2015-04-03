a5872f5a80e1695d0819b809bb2fc0f6a09b1413
qmillegan <qmillegan@users.noreply.github.com>
2015-04-03 11:25:25 -0700

    commented the code

--- batterytester.ino
+++ batterytester.ino
@@ -14,19 +14,26 @@ void loop()
 {
   analogValue = analogRead(0);
   voltage = 0.0048*analogValue;
-  if ( voltage >= 1.6 )
+/*
+because analogRead return a value between 0 and 1023.
+1023 = 5V
+0 = 0V
+so to convert the analogRead's value to volt 
+you need to multiply analogValue by 5/1023 (0.00488)
+*/
+  if ( voltage >= 1.6 )  // NEW BLUE LED
  {
    digitalWrite(newLED, HIGH);
    delay(ledDelay);
    digitalWrite(newLED, LOW);
  }
- else if ( voltage < 1.6 && voltage > 1.4 )
+ else if ( voltage < 1.6 && voltage > 1.4 ) // OK PEACHISH LED
  {
    digitalWrite(okLED, HIGH);
    delay(ledDelay);
    digitalWrite(okLED, LOW);
  }
- else if ( voltage <= 1.4 )
+ else if ( voltage <= 1.4 ) // OLD RED LED
  {
    digitalWrite(oldLED, HIGH);
    delay(ledDelay);
